58de551454dcb5cbd42cd0cdc66d7c20553fd8be
StikyPiston <156162907+StikyPiston@users.noreply.github.com>
2026-01-02 16:43:08 +0200

    Add configurations and hSpec for penguin

---
+++ config/nvim.penguin/init.lua
@@ -0,0 +1,352 @@
+-- Shorthands
+local map  = vim.keymap.set
+local opts = { noremap = true, silent = true }
+local noop = function() end
+
+-- Options
+vim.o.signcolumn     = "yes"
+vim.o.wrap           = false
+vim.o.tabstop        = 4
+vim.o.number         = true
+vim.o.smartindent    = true
+vim.o.swapfile       = false
+vim.o.winborder      = "rounded"
+vim.o.clipboard      = "unnamedplus"
+vim.o.mousemoveevent = true
+vim.o.number         = true
+vim.o.shiftwidth     = 4
+vim.g.mapleader      = " "
+vim.o.foldlevel      = 99
+vim.o.foldlevelstart = 99
+
+-- Unbind things
+map({ "n", "v", "o" }, "<Up>",    noop)
+map({ "n", "v", "o" }, "<Down>",  noop)
+map({ "n", "v", "o" }, "<Left>",  noop)
+map({ "n", "v", "o" }, "<Right>", noop)
+map("n",               "v",       "<C-v>")
+
+map({ "n", "v", "o" }, "h", noop)
+map({ "n", "v", "o" }, "j", noop)
+map({ "n", "v", "o" }, "k", noop)
+map({ "n", "v", "o" }, "l", noop)
+map({ "n", "v", "o" }, "i", noop)
+
+-- Bind IJKL as movement keys
+map({ "n", "v", "o" }, "i", "k")
+map({ "n", "v", "o" }, "k", "j")
+map({ "n", "v", "o" }, "j", "h")
+map({ "n", "v", "o" }, "l", "l")
+
+-- Bindings
+map('n', ';',          'i', opts)
+map('i', '<A-o>',      '<C-x><C-o>', opts)
+map('n', 'tT',         ':Oil<CR>')
+map('n', 'fF',         function() require("telescope.builtin").find_files() end)
+map('n', 'fG',         function() require("telescope.builtin").live_grep() end)
+map('n', 'dD',         ':Dashboard<CR>')
+map('n', 'pP',         ':RenderMarkdown toggle<CR>')
+map('n', 'hH',         ':tabNext<CR>')
+map('n', 'hE',         ':tabnew<CR>')
+map('n', 'gO',         ':Obsidian')
+map('n', 'hK',         vim.cmd.split)
+map('n', 'hL',         vim.cmd.vsplit)
+map('n', '<C-Up>',     '<C-w>k')
+map('n', '<C-Down>',   '<C-w>j')
+map('n', '<C-Left>',   '<C-w>h')
+map('n', '<C-Right>',  '<C-w>l')
+map('n', '<C-s>',      function() require('flash').jump() end)
+map('n', '<C-i>',      ':FzfNerdfont<CR>')
+map('n', '<C-b>',      function() vim.pack.update() end)
+map('n', '<leader>ow', function() require("neowiki").open_wiki() end)
+map('n', '<leader>oW', function() require("neowiki").open_wiki_floating() end)
+map('n', '<leader>tp', ':Triforce profile<CR>')
+map('n', '<leader>lg', ':LazyGit<CR>')
+map('n', '<leader>w',  ':w<CR>')
+map('n', '<leader>q',  ':q<CR>')
+map('n', '<leader>so', ':so<CR>')
+map('n', '<leader>lf', vim.lsp.buf.format)
+map('n', '<leader>cf', function() require("coinflip").flip() end)
+map('n', '<leader>mo', ':MarkmapOpen')
+map('n', '<leader>ms', ':MarkmapSave')
+map('n', '<leader>r',  ':%s/')
+map('n', '<leader>sp', ':StudytoolsPomodoro 25 5<CR>')
+map('n', '<leader>sP', ':StudytoolsPomodoroStatus<CR>')
+map('n', '<leader>bb', ':StudytoolsBlurt<CR>')
+map('n', '<C-Up>',     function() require("multicursor-nvim").lineAddCursor(-1) end)
+map('n', '<C-Down>',   function() require("multicursor-nvim").lineAddCursor(1) end)
+map('n', ',',          function() require("multicursor-nvim").clearCursors() end)
+
+-- Packing it up in here :P
+vim.pack.add({
+	"https://github.com/catppuccin/nvim",
+	"https://github.com/nvim-treesitter/nvim-treesitter",
+	"https://github.com/neovim/nvim-lspconfig",
+	"https://github.com/mason-org/mason.nvim",
+	"https://github.com/nvim-tree/nvim-web-devicons",
+	"https://github.com/hrsh7th/nvim-cmp",
+	"https://github.com/nvim-lua/plenary.nvim",
+	"https://github.com/stevearc/oil.nvim",
+	"https://github.com/nvim-mini/mini.nvim",
+	"https://github.com/MeanderingProgrammer/render-markdown.nvim",
+	"https://github.com/folke/flash.nvim",
+	"https://github.com/gisketch/triforce.nvim",
+	"https://github.com/nvzone/volt",
+	"https://github.com/lukas-reineke/indent-blankline.nvim",
+	"https://github.com/MunifTanjim/nui.nvim",
+	"https://github.com/soulis-1256/eagle.nvim",
+	"https://github.com/stephansama/fzf-nerdfont.nvim",
+	"https://github.com/ibhagwan/fzf-lua",
+	"https://github.com/nvzone/typr",
+	"https://github.com/apple/pkl-neovim",
+	"https://github.com/charmbracelet/tree-sitter-vhs",
+	"https://github.com/echaya/neowiki.nvim",
+	"https://github.com/nvim-lualine/lualine.nvim",
+	"https://github.com/dstein64/vim-startuptime",
+	"https://github.com/brenoprata10/nvim-highlight-colors",
+	"https://github.com/kdheepak/lazygit.nvim",
+	"https://github.com/StikyPiston/coinflip.nvim",
+	"https://github.com/NStefan002/2048.nvim",
+	"https://github.com/sqwxl/playdate.nvim",
+	"https://github.com/Zeioth/markmap.nvim",
+	"https://github.com/saghen/blink.cmp",
+	"https://github.com/folke/lazydev.nvim",
+	"https://github.com/piersolenski/skifree.nvim",
+	"https://github.com/chentoast/marks.nvim",
+	"https://github.com/stikypiston/cheaty.nvim",
+	"https://github.com/lewis6991/gitsigns.nvim",
+	"https://github.com/m4xshen/autoclose.nvim",
+	"https://github.com/stikypiston/studytools.nvim",
+	"https://github.com/nvim-telescope/telescope.nvim",
+	"https://github.com/folke/snacks.nvim",
+	"https://github.com/chrisgrieser/nvim-origami",
+	"https://github.com/folke/which-key.nvim",
+	"https://github.com/jake-stewart/multicursor.nvim"
+})
+
+-- Treesitter Setup
+require("nvim-treesitter").install({ 'c', 'lua', 'swift', 'ruby', 'hyprlang', 'bash', 'go', 'gomod', 'gosum', 'kdl', 'markdown', 'markdown_inline', 'python', 'vhs', 'html', 'latex', 'yaml', 'typst', 'zsh' })
+vim.api.nvim_create_autocmd('FileType', {
+	pattern = { '<filetype>' },
+	callback = function() vim.treesitter.start() end
+})
+vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+
+-- Theming
+vim.cmd.colorscheme "catppuccin-mocha"
+vim.api.nvim_set_hl(0, "Normal",      { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalNC",    { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+vim.api.nvim_set_hl(0, "LineNr",      { bg = "#1e2030" })
+vim.api.nvim_set_hl(0, "SignColumn",  { bg = "#1e2030" })
+
+-- Plugin Setup
+require("mason").setup()
+require("mini.icons").setup()
+require("mini.tabline").setup()
+require("mini.starter").setup()
+require("mini.notify").setup()
+require("oil").setup()
+require("render-markdown").setup()
+require('ibl').setup()
+require('eagle').setup({
+	keyboard_mode = true,
+	mouse_mode    = true
+})
+require("neowiki").setup({
+	wiki_dirs = {
+		{ name = "School", path = "~/Notebooks/School" }
+	}
+})
+require("lualine").setup({
+	options = {
+		section_separators   = { right = "", left = "" },
+		compenent_separators = { right = "", left = "" }
+	},
+
+	sections = {
+		lualine_a = { "mode" },
+		lualine_b = { "filename" },
+		lualine_c = {},
+		lualine_x = { require("triforce.lualine").level, "filetype", "location", "diff" },
+		lualine_y = { "lsp_status", "diagnostics" },
+		lualine_z = {}
+	}
+})
+require("triforce").setup()
+require("nvim-highlight-colors").setup({})
+require("2048").setup()
+require("playdate").setup({
+	playdate_sdk_path = "/home/distrorockhopper/Documents/PlaydateSDK-3.0.2",
+	build = {
+		source_dir = "src",
+		output_dir = "build.pdx"
+	}
+})
+require("markmap").setup({
+	html_output  = "/tmp/markmap.html",
+	hide_toolbar = false,
+	grace_period = 3600000
+})
+require("blink.cmp").setup()
+require("lazydev").setup()
+require("marks").setup({
+	mappings = {
+		set_next = "m,",
+		next     = "m>",
+		prev     = "m<"
+	},
+
+	builtin_marks = { "." }
+})
+require("cheaty").setup({
+	keymap = "<leader>cs",
+	cheatsheet = {
+		"# Cheatsheet",
+		"",
+		"## Basics",
+		"- gq         : Wrap to 80-character long lines",
+		"- : [visual] : do the '<,'> thing",
+		"- g??        : ROT13 the current line for some reason",
+		"- <leader>r  : :%s/",
+		"- ~          : Toggle case of character under cursor",
+		"",
+		"## Marks",
+		"- m[letter]  : Create mark of that letter",
+		"- m,         : Create next available mark",
+		"- '[letter]  : Go to mark of that letter",
+		"- m<         : Go to previous mark",
+		"- m>         : Go to next mark",
+		"- m.         : Go to last-edited line",
+		"- :delmark x : Delete mark of that letter",
+		"",
+		"## NeoWiki",
+		"- <leader>ow : Open NeoWiki",
+		"- <leader>oW : Open floating NeoWiki",
+		"- <leader>ms : Open Mindmap of current file",
+		"",
+		"## Studytools",
+		"- <leader>sp : Start Pomodoro timer (25/5 minute intervals)",
+		"- <leader>sP : Pomodoro timer status",
+		"- <leader>bb : Start blurting buffer",
+		"",
+		"## Folding",
+		"- zc         : Fold",
+		"- zM         : Fold all",
+		"- zo         : Unfold",
+		"- zO         : Unfold all under cursor",
+		"- zr         : Unfold all one level",
+		"- zf         : Create fold",
+		"- za         : Toggle fold under cursor"
+	}
+})
+require("autoclose").setup()
+require("studytools.inlineannotations").setup()
+require("studytools.pomodoro").setup()
+require("studytools.blurt").setup()
+require("snacks").setup({
+	image     = {},
+	quickfile = {}
+})
+require("origami").setup({
+	autoFold = {
+		enabled = false
+	}
+})
+require("which-key").setup({
+	preset = "helix",
+	layout = {
+		align = "right"
+	}
+})
+require("multicursor-nvim").setup()
+
+-- Hijinks in LSP land
+
+-- > Lua language server
+vim.lsp.config("lua_ls", {
+	cmd = { '/usr/sbin/lua-language-server' },
+	filetypes = { 'lua' },
+	root_markers = {
+		'.luarc.json',
+		'.luarc.jsonc',
+		'.luacheckrc',
+		'.stylua.toml',
+		'stylua.toml',
+		'selene.toml',
+		'selene.yml',
+		'.git',
+	},
+})
+vim.lsp.enable("lua_ls")
+
+-- > Ruby LSP
+vim.lsp.config("ruby-lsp", {
+	cmd          = { "ruby-lsp" },
+	filetypes    = { 'ruby', 'eruby' },
+	root_markers = { 'Gemfile', '.git' },
+	init_options = { formatter = 'auto', },
+	reuse_client = function(client, config)
+		config.cmd_cwd               =  config.root_dir
+		return client.config.cmd_cwd == config.cmd_cwd
+	end,
+})
+vim.lsp.enable("ruby-lsp")
+
+-- > Crystal LSP
+vim.lsp.config("crystalline", {
+	cmd          = { 'crystalline' },
+	filetypes    = { 'crystal' },
+	root_markers = { 'shard.yml', '.git' }
+})
+vim.lsp.enable("crystalline")
+
+-- > Hyprlang LSP
+vim.lsp.config('hyprls', {
+	cmd          = { 'hyprls', '--stdio' },
+	filetypes    = { 'hyprlang' },
+	root_markers = { '.git' }
+})
+vim.lsp.enable("hyprls")
+
+-- > Fish LSP
+vim.lsp.config("fish-lsp", {
+	cmd          = { 'fish-lsp', 'start' },
+	filetypes    = { 'fish' },
+	root_markers = { 'config.fish', '.git' },
+})
+vim.lsp.enable("fish-lsp")
+
+-- > CoffeeScript LSP
+vim.lsp.config("coffeesense", {
+	cmd          = { 'coffeesense-language-server', '--stdio' },
+	filetypes    = { 'coffee' },
+	root_markers = { 'package.json' },
+})
+vim.lsp.enable("coffeesense")
+
+-- > Rust LSP
+vim.lsp.enable("rust_analyzer")
+
+-- > Zig LSP
+vim.lsp.enable("zls")
+
+-- > MCFunction LSP
+vim.lsp.enable("spyglassmc_language_server")
+
+-- > Typst LSP
+vim.lsp.enable("tinymist")
+
+-- > Python LSP
+vim.lsp.enable("pyright")
+
+-- > Swift LSP
+vim.lsp.enable("sourcekit")
+
+-- > Go LSP
+vim.lsp.enable("gopls")
+
+-- Inline diagnostics
+vim.diagnostic.config({
+	virtual_text = true
+})
