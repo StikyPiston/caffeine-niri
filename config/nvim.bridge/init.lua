e93fc1f14a228f718899b021d6caafb0eaf9c425
StikyPiston <156162907+StikyPiston@users.noreply.github.com>
2026-01-07 12:09:02 +0200

    Replace mini.notify with snacks.nvim notifier

--- config/nvim.bridge/init.lua
+++ config/nvim.bridge/init.lua
@@ -110,7 +110,6 @@ require("lazy").setup({
 		{ "stevearc/oil.nvim", config = function() require("oil").setup() end },
 		{ "nvim-mini/mini.nvim", config = function()
 				require("mini.icons").setup()
-				require("mini.notify").setup()
 			end
 		},
 		{ "oxy2dev/markview.nvim" },
@@ -237,6 +236,7 @@ require("lazy").setup({
 				image     = { enabled = true },
 				quickfile = { enabled = true },
 				scroll    = { enabled = true, animate = { duration = { step = 1, total = 10 } } },
+				notifier  = { enabled = true },
 			}
 		},
 		{ "chrisgrieser/nvim-origami", event = "VeryLazy", opts = {
@@ -319,6 +319,48 @@ dash.section.buttons.val = {
 	dash.button( "q", "󰩈 Exit Neovim", ":qa<CR>" )
 }
 alpha.setup(dash.opts)
+
+local progress = vim.defaulttable()
+vim.api.nvim_create_autocmd("LspProgress", {
+  callback = function(ev)
+    local client = vim.lsp.get_client_by_id(ev.data.client_id)
+    local value = ev.data.params.value
+    if not client or type(value) ~= "table" then
+      return
+    end
+    local p = progress[client.id]
+
+    for i = 1, #p + 1 do
+      if i == #p + 1 or p[i].token == ev.data.params.token then
+        p[i] = {
+          token = ev.data.params.token,
+          msg = ("[%3d%%] %s%s"):format(
+            value.kind == "end" and 100 or value.percentage or 100,
+            value.title or "",
+            value.message and (" **%s**"):format(value.message) or ""
+          ),
+          done = value.kind == "end",
+        }
+        break
+      end
+    end
+
+    local msg = {}
+    progress[client.id] = vim.tbl_filter(function(v)
+      return table.insert(msg, v.msg) or not v.done
+    end, p)
+
+    local spinner = { "/", "-", "\\", "|" }
+    vim.notify(table.concat(msg, "\n"), "info", {
+      id = "lsp_progress",
+      title = client.name,
+      opts = function(notif)
+        notif.icon = #progress[client.id] == 0 and " "
+          or spinner[math.floor(vim.uv.hrtime() / (1e6 * 80)) % #spinner + 1]
+      end,
+    })
+  end,
+})
 
 -- Hijinks in LSP land
 
