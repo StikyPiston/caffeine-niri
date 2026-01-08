df9baeed31cf49fbd6e0c494ac4f6c566edbc31e
StikyPiston <156162907+StikyPiston@users.noreply.github.com>
2026-01-08 15:27:07 +0200

    Enable spellcheck in Markdown files

--- config/nvim.bridge/init.lua
+++ config/nvim.bridge/init.lua
@@ -360,8 +360,9 @@ vim.api.nvim_create_autocmd("LspProgress", {
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "markdown",
   callback = function()
-    vim.opt_local.wrap = true
+    vim.opt_local.wrap      = true
     vim.opt_local.linebreak = true
+	vim.opt_local.spell     = true
   end,
 })
 
